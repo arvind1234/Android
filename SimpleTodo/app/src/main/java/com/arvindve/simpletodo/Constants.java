66a85803e5a0197a0276024a404f558b53323275
Arvind Venkataraman <arvindve@microsoft.com>
2018-01-29 19:29:23 -0800

    Backup of todo app

---
+++ SimpleTodo/app/src/main/java/com/arvindve/simpletodo/Constants.java
@@ -0,0 +1,10 @@
+package com.arvindve.simpletodo;
+
+/**
+ * Created by arvindve on 8/21/17.
+ */
+
+public final class Constants {
+    static final String ItemText = "itemtext";
+    static final String ItemPosition = "itemposition";
+}
